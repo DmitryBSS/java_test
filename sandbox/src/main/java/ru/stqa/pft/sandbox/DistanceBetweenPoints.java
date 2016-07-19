ec69b0c86ce3f8891d7197265af534b0cdeb472f
DmitryBSS <d.v.popov@inbox.ru>
2016-07-19 12:29:05 +0300

    Задание №2. Пункты 1-3(fix).

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBetweenPoints.java
@@ -7,7 +7,9 @@ public class DistanceBetweenPoints {
 
   public static void main(String[] args) {
 
+    Point p1 = new Point(2, 2);
     Point p2 = new Point(2, 4);
+    System.out.println("Расстояние между точками p1(" + p1.x + ", " + p1.y + ") и p2(" + p2.x + ", " + p2.y + ") = " + distance(p1, p2));
   }
 
   public static double distance(Point p1, Point p2) {
