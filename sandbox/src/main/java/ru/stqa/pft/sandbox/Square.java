c9382b2ca6d9f922af4cafc81d92aed7a99a9b2d
DmitryBSS <d.v.popov@inbox.ru>
2016-07-21 14:59:31 +0300

    Изменения из первой лекции

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by popdv on 19.07.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+}
