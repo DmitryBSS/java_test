7182c950ee559c740155e6b2b5031b8af8d3d9fc
DmitryBSS <d.v.popov@inbox.ru>
2016-08-04 18:37:39 +0300

    sandbox - Коллекции: наборы элементов

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,34 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by popdv on 04.08.2016.
+ */
+public class Collections {
+
+    public static void main(String[] args) {
+        String[] langs = {"Java", "C#", "Python", "PHP"};
+
+        List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+        for (String l : languages) {
+            System.out.println("Я хочу выучить " + l);
+        }
+
+        for (int i=0; i < languages.size(); i++) {
+            System.out.println("Я хочу выучить " + languages.get(i));
+        }
+//        List<String> languages = new ArrayList<String>();
+//        languages.add("Java");
+//        languages.add("C");
+//        languages.add("Python");
+//        languages.add("PHP");
+
+//        for (String l : languages) {
+//            System.out.println("Я хочу выучить " + l);
+//        }
+    }
+}
