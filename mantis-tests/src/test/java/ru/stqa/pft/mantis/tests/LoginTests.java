372c79f8552f74326b115caa802e750ed5b8c60b
popdv <d.popov@bssys.com>
2016-09-02 18:46:16 +0300

    Тест - проверка авторизации

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+
+/**
+ * Created by popdv on 02.09.2016.
+ */
+public class LoginTests extends TestBase {
+
+    @Test
+    public void testLogin() throws IOException {
+        HttpSession session = app.newSession();
+        assertTrue(session.login("administrator", "12345"));
+        assertTrue(session.isLoggedInAs("administrator"));
+    }
+}
