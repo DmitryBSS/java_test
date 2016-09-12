84c76b2dd33f908c3916a7fe361f66ba01b7b659
popdv <d.popov@bssys.com>
2016-09-12 18:55:45 +0300

    fix

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactEmailTests.java
@@ -4,6 +4,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -18,9 +19,15 @@ public class ContactEmailTests extends TestBase {
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().homePage();
-        if (app.contract().all().size() == 0) {
+        if (app.db().contacts().size() == 0) {
             app.goTo().gotoContactPage();
-            app.contract().create(new ContactData().withFirstName("Dmitry").withMiddleName("Victorovich").withLastName("Popov").withNickName("popdv").withTitle("Title").withCompany("BSS").withAddress("Moscow Nagornyi p-d").withTelHome("+7(495)111-11-11").withTelMobile("+79295622211").withTelWork("+7(495)111-11-11").withFax("+7(495)111-11-13").withEmail1("asd@asd.ru").withEmail2("asd2@asd.ru").withEmail3("asd3@asd.ru").withHomePage("www.asdsa.ru").withbYear("1990").withaYear("2007").withAddress2("Moscow, Filevskyi bil").withPhone2("+7(495)123-11-22").withNotes("заметки"));
+            File photo = new File("src/test/resources/stru.png");
+            app.contract().create(new ContactData()
+                    .withFirstName("Dmitry").withMiddleName("Victorovich").withLastName("Popov").withNickName("popdv")
+                    .withTitle("Title").withCompany("BSS").withAddress("Moscow Nagornyi p-d").withTelHome("+7(495)111-11-11")
+                    .withTelMobile("+79295622211").withTelWork("+7(495)111-11-11").withFax("+7(495)111-11-13").withEmail1("asd@asd.ru")
+                    .withEmail2("asd2@asd.ru").withEmail3("asd3@asd.ru").withHomePage("www.asdsa.ru").withbYear("1990").withaYear("2007")
+                    .withAddress2("Moscow, Filevskyi bil").withPhone2("+7(495)123-11-22").withNotes("заметки").withPhoto(photo));
         }
     }
 
